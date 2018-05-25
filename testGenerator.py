5784cad6f64e74106e759c3f4f681ff7ccf2fe3e
Nathan Larson <nathanlarson3141@gmail.com>
2018-05-25 03:01:38 -0400

    fixing

--- testGenerator.py
+++ testGenerator.py
@@ -4,8 +4,8 @@ from pywikibot import pagegenerators
 site = pywikibot.Site()
 cat = pywikibot.Category(site,'Category:Candidates for speedy deletion')
 stuff = cat.articles()
-for attr in dir(stuff):
-    print("stuff.%s = %r" % (attr, getattr(stuff, attr)))
+#for attr in dir(stuff):
+#    print("stuff.%s = %r" % (attr, getattr(stuff, attr)))
 for page in stuff:
-	title = stuff.title
-	print(title)
+	lastrevid = page.lastrevid
+	print(lastrevid)
